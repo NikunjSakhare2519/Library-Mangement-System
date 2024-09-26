06641aa01c3a8827a1f06226b7de82620e5ee855
NikunjSakhare2519 <nikunjsakhare25@gmail.com>
2024-09-27 02:28:25 +0530

    "Borrow book frame is created which have book id , student id , issue date , return date fields and data from those fields is inserted into issue table of lms database"

--- src/home.java
+++ src/home.java
@@ -117,6 +117,7 @@ public class home extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        new issueBook().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
