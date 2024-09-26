edfb44330f9b95f29e24ff97fb3c251ffeaf5eb9
NikunjSakhare2519 <nikunjsakhare25@gmail.com>
2024-09-27 01:31:22 +0530

    "Add new book frame created where bookID which is unique , book name , publisher name, price of book, published book year is taken from input and stored in SQL Database table named Book"

--- src/home.java
+++ src/home.java
@@ -47,6 +47,11 @@ public class home extends javax.swing.JFrame {
         jButton2.setBackground(new java.awt.Color(0, 255, 102));
         jButton2.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
         jButton2.setText("ADD BOOK");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setBackground(new java.awt.Color(255, 51, 51));
         jButton3.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
@@ -125,6 +130,11 @@ public class home extends javax.swing.JFrame {
         new newStudent().setVisible(true);
         
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        new newBook().setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
